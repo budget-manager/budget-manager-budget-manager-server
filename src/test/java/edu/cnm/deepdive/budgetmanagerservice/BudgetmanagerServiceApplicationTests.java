3269d7e7c1b3d5af68e01ef9c1d16efa6dd5c904
Mercedes <mercedes.nmx@gmail.com>
2020-06-24 14:12:42 -0600

    Initial commit

---
+++ src/test/java/edu/cnm/deepdive/budgetmanagerservice/BudgetmanagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.budgetmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetmanagerServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
